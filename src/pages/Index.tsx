cc3065c86765238854cfd1a7791226492ca31392
tuyambaze gilbert <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2025-11-26 16:15:59 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,26 +5,11 @@ import Services from '@/components/Services';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
-import Head from 'next/head';
+
 
 
 export default function Index() {
-  const schema = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    name: "Gilbert Tuyambaze",
-    jobTitle: "Full-Stack Developer, Designer & Music Producer",
-    url: "https://tuyambaze-gilbert.vercel.app",
-    sameAs: [
-      "https://github.com/GILBERT-Tuyambaze",
-      "https://www.linkedin.com/in/gilbert-tuyambaze-6927ba336/",
-      "mailto:tuyambazegilbert05@gmail.com"
-    ],
-    worksFor: {
-      "@type": "Organization",
-      name: "Freelance / Independent",
-    },
-  };
+  
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Navigation />
